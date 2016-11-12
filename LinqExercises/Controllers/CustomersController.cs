a35857141996b80f82258a0f02797cd0446fc53b
bahmansadiq <bahman.sadiq@gmail.com>
2016-11-12 12:42:49 -0800

    Linq excercises

---
+++ LinqExercises/Controllers/CustomersController.cs
@@ -0,0 +1,78 @@
+﻿using LinqExercises.Infrastructure;
+using System;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace LinqExercises.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        private NORTHWNDEntities _db;
+
+        public CustomersController()
+        {
+            _db = new NORTHWNDEntities();
+        }
+
+        // GET: api/customers/city/London
+        [HttpGet, Route("api/customers/city/{city}"), ResponseType(typeof(IQueryable<Customer>))]
+        public IHttpActionResult GetAll(string city)
+        {
+            var specificCustomers = from customer in _db.Customers
+                                    where customer.City.Contains(city)
+                                    select customer;
+
+            return Ok(specificCustomers);
+            //var specificCustomersLambda = _db.Customers.Where(n => n.City.Contains(city));
+            //return Ok(specificCustomersLambda);
+            //   throw new NotImplementedException("Write a query to return all customers in the given city");
+        }
+
+        // GET: api/customers/mexicoSwedenGermany
+        [HttpGet, Route("api/customers/mexicoSwedenGermany"), ResponseType(typeof(IQueryable<Customer>))]
+        public IHttpActionResult GetAllFromMexicoSwedenGermany()
+        {
+            var cities = from customer in _db.Customers
+                         where (customer.Country == "Mexico" || customer.Country== "Sweden" || customer.Country== "Germany")
+                         select customer;
+            return Ok(cities);
+
+            //var citiesLambda = _db.Customers.Where(n => n.Country == "Mexico" || n.Country == "Sweden" || n.Country == "Germany");
+            //return Ok(citiesLambda);
+
+           // throw new NotImplementedException("Write a query to return all customers from Mexico, Sweden and Germany.");
+        }
+
+        // GET: api/customers/shippedUsing/Speedy Express
+        [HttpGet, Route("api/customers/shippedUsing/{shipperName}"), ResponseType(typeof(IQueryable<Customer>))]
+        public IHttpActionResult GetCustomersThatShipWith(string shipperName)
+        {
+            var custList = from customer in _db.Customers
+                          // where customer.Orders.Contains(shipperName)        
+                           select customer;
+            return Ok(custList);
+            
+                        
+             
+          //  throw new NotImplementedException("Write a query to return all customers with orders that shipped using the given shipperName.");
+        }
+
+        // GET: api/customers/withoutOrders
+        [HttpGet, Route("api/customers/withoutOrders"), ResponseType(typeof(IQueryable<Customer>))]
+        public IHttpActionResult GetCustomersWithoutOrders()
+        {
+
+            var listCustomersWithoutOrders = from customer in _db.Customers
+                                             where customer.Orders.Equals(null)
+                                             select customer;
+            return Ok(listCustomersWithoutOrders);
+            //throw new NotImplementedException("Write a query to return all customers with no orders in the Orders table.");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+        }
+    }
+}
---
+++ LinqExercises/Controllers/ProductsController.cs
@@ -0,0 +1,72 @@
+﻿using LinqExercises.Infrastructure;
+using System;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace LinqExercises.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private NORTHWNDEntities _db;
+
+        public ProductsController()
+        {
+            _db = new NORTHWNDEntities();
+        }
+
+        //GET: api/products/discontinued/count
+        [HttpGet, Route("api/products/discontinued/count"), ResponseType(typeof(int))]
+        public IHttpActionResult GetDiscontinuedCount()
+        {
+
+            //  Select SUM(CAST(Discontinued AS INT)) from Products
+            int product = _db.Products
+                           .Where(p => p.Discontinued == true)
+                           .Select(p=> p).Count();
+                           
+
+
+           return Ok(product);
+            //throw new NotImplementedException("Write a query to return the number of discontinued products in the Products table.");
+        }
+
+        // GET: api/categories/Condiments/products
+        [HttpGet, Route("api/categories/{categoryName}/products"), ResponseType(typeof(IQueryable<Product>))]
+        public IHttpActionResult GetProductsInCategory(string categoryName)
+        {
+            //select c.CategoryID, c.CategoryName from Categories c right Join Products p
+            //  on c.CategoryID = p.ProductID
+            //Where c.CategoryName = 'Condiments'
+            var category = from C in _db.Categories
+                           join p in _db.Products
+                           on C.CategoryID equals p.ProductID
+                           where (C.CategoryName.Contains(categoryName))
+                           select(new
+                           {
+                               products=C.CategoryName
+                           });
+                
+                
+            return Ok(category);
+
+
+
+
+            //throw new NotImplementedException("Write a query to return all products that fall within the given categoryName.");
+        }
+
+        // GET: api/products/reports/stock
+        [HttpGet, Route("api/products/reports/stock"), ResponseType(typeof(IQueryable<object>))]
+        public IHttpActionResult GetStockReport()
+        {
+            // See this blog post for more information about projecting to anonymous objects. https://blogs.msdn.microsoft.com/swiss_dpe_team/2008/01/25/using-your-own-defined-type-in-a-linq-query-expression/
+            throw new NotImplementedException("Write a query to return an array of anonymous objects that have two properties. A Product property and the total units in stock for each product labelled as 'TotalStockUnits' where TotalStockUnits is greater than 100.");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+        }
+    }
+}
